08f4ba554295aa87736116de7d844bf269b7b45e
AhmedOmarKhattab <ahmmmedomarkhtab@gmail.com>
2026-03-03 21:56:04 +0200

    Change logo

--- OnlineShop/obj/Debug/net8.0/OnlineShop.AssemblyInfo.cs
+++ OnlineShop/obj/Debug/net8.0/OnlineShop.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2884569b6c3bd4fb10c58adca1e60de6fdb8f0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+635602488290370ce96cbe34b41484bcfd402a97")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
